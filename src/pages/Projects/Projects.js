d7e76e3fb0459da6145ce5fecf90e2bbc6e4ac41
JaimePintoP <jaimepinto.saxo@gmail.com>
2021-08-11 18:34:23 +0200

    Add ProjectCard component to Projects page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,7 +1,13 @@
 import React from "react";
+import ProjectCard from "../../components/Cards/ProjectCard/ProjectCard";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      Projects
+      <ProjectCard />
+    </div>
+  );
 };
 
 export default Projects;
